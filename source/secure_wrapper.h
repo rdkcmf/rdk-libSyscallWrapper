8ce7f3e0db1acb8b59365ba036b3dcb78a848935
Mike Baker <Michael_Baker2@comcast.com>
2019-02-08 13:37:56 -0800

    RDK-24539 : system wrapper rewrite
    Reason for change: rewrite
    Test Procedure: verify regression
    Risks: low
    
    Signed-off-by: Mike Baker <Michael_Baker2@comcast.com>
    (cherry picked from commit 007c5cd9ba08d149bb211856fdfa263983f41e3a)
    
    RDK-24539 : system wrapper vsnprintf fix
    
    Reason for change: fix a platform specific issue with vsnprintf
    Test Procedure: verify regression
    Risks: low
    
    Change-Id: I4caa58c6731d12e9e1edaef17453caadeb9feffd
    Signed-off-by: Mike Baker <Michael_Baker2@comcast.com>
    (cherry picked from commit f760fdefdc7b9410c02f86427907134aa228fcd5)
    (cherry picked from commit e45022169c3602b0ec129ca5cd4593fbc739ae05)
    (cherry picked from commit a9fa3b0b0fb57ca20cc0c0b85c756fc6fd8f9b1a)
    (cherry picked from commit b98f0bde2ff95519c09c5f74150a3ea9ce7aa05c)
    (cherry picked from commit 8ec5fefce25550612304ee569c8c7b33e2eb4d5f)
    (cherry picked from commit a74b3dccb56438380334460b6467e9e8da7bd970)

--- source/secure_wrapper.h
+++ source/secure_wrapper.h
@@ -1,145 +1,79 @@
-/*
- * If not stated otherwise in this file or this component's Licenses.txt file the
- * following copyright and licenses apply:
- *
- * Copyright 2018 RDK Management
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
-#ifndef SYSCALL_H
-#define SYSCALL_H
-/**********************************************************************
-    prototype:
-        int secure_system_call_p( const char *cmd, char *argp[]);
+#ifndef __SECURE_H
+#  define __SECURE_H
+#  ifdef __cplusplus
+extern "C" {
+#  endif
 
-    Description:
+__attribute__((nonnull))
+__attribute__((format(printf,1,2)))
+int v_secure_system(const char *command, ...);
 
-        Implementation of secure system call which takes parameters as
-        array.
+__attribute__((nonnull))
+__attribute__((format(printf,2,3)))
+FILE *v_secure_popen(const char *direction, const char *command, ...);
 
-    Arguments:    const char *                 cmd
-                  command to be executed.
+__attribute__((nonnull))
+int v_secure_pclose(FILE *);
 
-                  char *                       argp[]
-                  list of parameters to be passed to command
-                  argp[0] : must be the command to be executed (cmd)
-                  argp[n] : last parameter must be NULL
+/* OBSOLETE CONSTANTS */
+#define _SPIPE "|"
+#define _SOR "||"
+#define _SAND "&&"
+#define _SBG "&"
+#define _STHEN ";"
 
-    Return:       The status of the operation. -1:Failure, 0:Success
-**********************************************************************/
+/* The following is just some gcc magic to make sure
+ * 1) the format string isn't a variable
+ * 2) the number of arguments matches the format
+ * 3) popen's direction arg is "r" or "w"
+ */
+#define v_secure_system(fmt, args...) \
+	({ \
+		int ret; \
+		_Pragma("GCC diagnostic push") \
+		_Pragma("GCC diagnostic error \"-Wformat\"") \
+		_Pragma("GCC diagnostic error \"-Wformat-security\"") \
+		if (!__builtin_constant_p(fmt)) { \
+		extern void format_error() __attribute__((error("command argument cannot be a variable\nreplace \"sprintf(buffer, command, args); v_secure_system(buffer);\" with \"v_secure_system(command, args);\""))); \
+			format_error(); \
+		} \
+		ret = v_secure_system(fmt, ##args); \
+		_Pragma("GCC diagnostic pop") \
+		ret; \
+	})
 
-int secure_system_call_p( const char *cmd, char *argp[]);
+#define v_secure_popen(direction, fmt, args...) \
+	({ \
+		FILE *ret; \
+		_Pragma("GCC diagnostic push") \
+		_Pragma("GCC diagnostic error \"-Wformat\"") \
+		_Pragma("GCC diagnostic error \"-Wformat-security\"") \
+		if ( \
+			__builtin_constant_p(*direction) && \
+			((direction[0] != 'r' && direction[0] != 'w') || direction[1] != '\0') \
+		) { \
+			extern void popen_check() __attribute__((error("v_secure_popen(direction, command, ...) direction must be \"r\" or \"w\""))); \
+			popen_check(); \
+		} \
+		if (!__builtin_constant_p(fmt)) { \
+			extern void format_error() __attribute__((error("command argument cannot be a variable"))); \
+			format_error(); \
+		} \
+		ret = v_secure_popen(direction, fmt, ##args); \
+		_Pragma("GCC diagnostic pop") \
+		ret; \
+	})
 
-/**********************************************************************
-    prototype:
-        int secure_system_call_vp( const char *cmd, ...);
-    Description:
-        Implementation of secure system call which takes variable
-        parameters.
-    Arguments:    const char *                 cmd
-                  command to be executed.
-                  ...
-                  variable number of arguments (char *)
-                  Last argument must be NULL
-    Return:       The status of the operation. -1:Failure, 0:Success
-**********************************************************************/
+extern int system(const char *command) __attribute__((warning("please replace system() with v_secure_system()")));
 
+extern int contains_secure_separator(char *str) __attribute__((warning("contains_secure_separator is obsolete")));
+extern int secure_system_call_p(const char *cmd, char *argv[]);
+extern int secure_system_call_vp(const char *cmd, ...);
 
-
-int secure_system_call_vp( const char *cmd, ...);
-
-/**********************************************************************
-    prototype:
-        int v_secure_system( const char* fmt , ...);
-    Description:
-        Implementation of  v_secure_system which takes variable
-        parameters.
-    Arguments:    const char *                 fmt
-                  command to be executed.
-                  ...
-                  variable number of arguments (char *)
-    Return:       The status of the operation. -1:Failure, 0:Success
-**********************************************************************/
-
-int v_secure_system(const char* fmt, ...);
-
-/**********************************************************************
-    prototype:
-        char* path_find( const char *arg);
-    Description:
-        Implementation of path_find which takes shell command name as
-        parameter.
-    Arguments:    const char *              arg
-                  
-    Return:       command with prepended path 
-**********************************************************************/
-
-//char* path_find(const char * arg);
-
-/**********************************************************************
-    prototype:
-       char* secure_popen_call_p( const char *cmd, char *argp[]);
-
-    Description:
-
-        Implementation of secure popen call which takes parameters as
-        array.
-
-    Arguments:    const char *                 cmd
-                  command to be executed.
-
-                  char *                       argp[]
-                  list of parameters to be passed to command
-                  argp[0] : must be the command to be executed (cmd)
-                  argp[n] : last parameter must be NULL
-
-    Return:       pointer to output buffer
-                  Note: Caller must free this buffer after use
-**********************************************************************/
-
-
-
-
-//Implementation of secure popen call which takes parameters as array
-//char* secure_popen_call_p( const char *cmd, char *argp[]);
-
-/**********************************************************************
-    prototype:
-        char* secure_popen_call_vp( const char *cmd, ...);
-
-    Description:
-
-        Implementation of secure popen call which takes variable
-        parameters.
-
-    Arguments:    const char *                 cmd
-                  command to be executed.
-
-                  ...
-                  variable number of arguments (char *)
-                  Last argument must be NULL
-
-    Return:       pointer to output buffer
-                  Note: Caller must free this buffer after use
-**********************************************************************/
-
-
-
-//char* secure_popen_call_vp( const char *cmd, ...);
-
-
-
-
+#  ifdef __cplusplus
+}
+#  endif
 #endif
-
--- testapp/test_secure_wrapper.c
+++ testapp/test_secure_wrapper.c
@@ -1,46 +1,45 @@
-/*
- * If not stated otherwise in this file or this component's Licenses.txt file the
- * following copyright and licenses apply:
- *
- * Copyright 2018 RDK Management
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
 #include <stdio.h>
-int main(int count, char *argv[]) {
-   FILE* fptr=NULL;
-   char output[1024];
-   fptr= v_secure_popen(argv[1]);
-   printf("\n ---output---\n");
-   if (fptr  == NULL) perror ("Error opening file");
-   else
-   {
-     while ( ! feof (fptr) )
-     {
-       if ( fgets (output , 1023 , fptr) == NULL ) break;
-       printf("%s", output);
-     }
-     v_secure_pclose(fptr);
-   }
-   printf("\n\n\n");
-   if(v_secure_system(argv[1])==0)
-   {
-     printf("OK");
-   }
-   else
-   {
-     printf("NOT OK");
-   }
-   printf("Testing Completed\n");
-   return 0;
+#include <stdlib.h>
+#include <string.h>
+#include "secure_wrapper.h"
+
+int main(/* int argc, char **argv */) {
+
+	v_secure_system("echo %s %s %s %s", "1", "2", "3", "4");
+	v_secure_system("echo %d `echo %d` %d %d", 1, 2, 3, 4);
+
+	v_secure_system("echo -n TEST01:; echo %s", "PASS");
+	v_secure_system("echo -n TEST02:; true  && echo PASS");
+	v_secure_system("echo -n TEST03:; false || echo PASS");
+	v_secure_system("echo -n TEST04:; true  && echo PASS || echo FAIL");
+	v_secure_system("echo -n TEST05:; echo PASS | grep PASS || echo FAIL");
+	v_secure_system("echo -n TEST06:; false && echo FAIL1 | echo FAIL2 || echo PASS");
+	v_secure_system("echo -n TEST07:; true  || echo FAIL1 | echo FAIL2 && echo PASS");
+	v_secure_system("echo -n TEST08:; echo FAIL  >/dev/null | grep FAIL || echo PASS");
+	v_secure_system("echo -n TEST09:; echo FAIL &>/dev/null | grep FAIL || echo PASS");
+	v_secure_system("echo -n TEST10:; ./FAIL 2>/dev/null || echo PASS");
+	v_secure_system("echo -n TEST11:; ./FAIL 2>&1 | grep FAIL >/dev/null && echo PASS");
+	v_secure_system("echo    TEST12:PASS > ./testfile; cat testfile");
+	v_secure_system("echo -n TEST13: > ./testfile; echo PASS >> testfile; cat < testfile");
+	v_secure_system("echo    TEST14:`echo FAIL >/dev/null; echo PASS`");
+	v_secure_system("echo -n TEST15:; echo %s`echo %s`%s | grep %s >/dev/null && echo PASS || echo FAIL", "1%s", "2%s", "3%s", "1%s2%s3%s");
+	v_secure_system("echo    TEST16:`%s 2>/dev/null || echo PASS`;", "echo FAIL");
+
+	// security checks
+	//v_secure_system("echo -n TESTxx:; echo FAIL >%s || echo PASS", "/dev/null");
+	
+	FILE *fp = v_secure_popen("w", "cat");
+	fprintf(fp, "popen write success\n");
+	fclose(fp);
+
+	char buf[1024];
+	memset(buf, 0, sizeof(buf));
+	fp = v_secure_popen("r", "echo popen read success");
+	buf[fread(buf, 1, sizeof(buf), fp)] = '\0';
+	fprintf(stdout, "%s", buf);
+	fclose(fp);
+
+	secure_system_call_vp("echo", "legacy", "api", "PASS", NULL);
+
+	return 0;
 }
